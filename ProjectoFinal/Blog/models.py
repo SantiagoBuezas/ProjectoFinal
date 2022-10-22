c1bc4b8343290d8ce2ef82697fcf85e43d2a1263
Santiago Buezas <sbues2@gmail.com>
2022-10-22 15:09:46 -0300

    creo Blog y la agrego al settings, y creo modelos iniciales

--- ProjectoFinal/Blog/models.py
+++ ProjectoFinal/Blog/models.py
@@ -1,40 +1,40 @@
 from django.db import models
 
-class Autor(models.Model):
 
+class Autor(models.Model):
     class Meta:
         verbose_name_plural = "Autores"
-    
+
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     profecion = models.CharField(max_length=50)
 
+
 def __str__(self):
     return f"{self.nombre} {self.apellido}"
 
-class secciones(models.Model):
 
+class secciones(models.Model):
     class Meta:
         verbose_name_plural = "Secciones"
-    
+
     nombre = models.CharField(max_length=50)
+
 
 def __str__(self):
     return f"{self.nombre}"
 
-class Articulo(models.Model):
 
+class Articulo(models.Model):
     class Meta:
-            verbose_name_plural = "Articulos"
+        verbose_name_plural = "Articulos"
 
     titulo = models.CharField(max_length=50)
-    
+
     texto = models.CharField(max_length=1000)
-    
+
     fecha = models.DateField(null=True)
+
 
 def __str__(self):
     return f"{self.titulo} ({self.fecha})"
-
-
-
