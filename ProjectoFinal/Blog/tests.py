4e312d9969fbc279955c16d1d9a769abc9f3be13
Santiago Buezas <sbues2@gmail.com>
2022-11-07 17:01:01 -0300

    agrego 3 test

--- ProjectoFinal/Blog/tests.py
+++ ProjectoFinal/Blog/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from Blog.models import Blog
 
-# Create your tests here.
+
+class ViewTestCase(TestCase):
+    def test_tema_de_blog(self):
+        Blog.objects.create(nombre="Camino de Pena", tema="Warhammer")
+        todos_los_blog = Blog.objects.all()
+        assert len(todos_los_blog) == 1
+        assert todos_los_blog[0].tema == "Warhammer"
+
+    def test_fecha_de_blog(self):
+        Blog.objects.create(
+            nombre="El Hobbit", tema="El señor de los anillos", fecha="10/10/2021"
+        )
+        todos_los_blog = Blog.objects.all()
+        assert todos_los_blog[0].fecha_de_inicio != None
+
+    def test_crear_4_blog(self):
+        Blog.objects.create(nombre="Camino a la Gloria", tema="Warhammer")
+        Blog.objects.create(nombre="Camino a la muerte", tema="Warhammer")
+        Blog.objects.create(nombre="La comunidad del anillo", tema="ESDA")
+        Blog.objects.create(nombre="las dos torres", tema="ESDA")
+        Blog.objects.create(nombre="El retorno del Rey", tema="ESDA")
+        todos_los_blogs = Blog.objects.all()
+        assert len(todos_los_blogs) == 4
